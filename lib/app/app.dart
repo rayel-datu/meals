5fdcec03b897151adc33757e3092dec7b5938aa3
Rayel Datu <rayeldatu@gmail.com>
2025-03-11 23:23:43 +0800

    chore(app_dependencies): Create Dependencies init helper

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uni_life_exam/app/utilities/dependencies.dart';
 
 class MealApp extends StatefulWidget {
   const MealApp({super.key});
@@ -8,6 +9,13 @@ class MealApp extends StatefulWidget {
 }
 
 class _MealAppState extends State<MealApp> {
+  @override
+  void initState() {
+    // initialize dependencies
+    Dependencies().init();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
---
+++ lib/app/utilities/dependencies.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:uni_life_exam/app/utilities/env_vars.dart';
+import 'package:uni_life_exam/features/categories/data/meal_api.dart';
+import 'package:uni_life_exam/features/categories/data/meal_api_impl.dart';
+
+class Dependencies {
+  void init() {
+    final baseUrl = EnvVars.getBaseUrl();
+    final mealDio = Dio(BaseOptions(baseUrl: baseUrl));
+    final mealApi = MealApiImpl(mealDio, baseUrl: baseUrl);
+
+    GetIt.instance.registerLazySingleton<MealApi>(() => mealApi);
+  }
+}
