b3747b7a7389b1bdd7ee4f0274ab7f1c80cebed7
Rayel Datu <rayeldatu@gmail.com>
2025-03-11 22:21:38 +0800

    initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MealApp extends StatefulWidget {
+  const MealApp({super.key});
+
+  @override
+  State<MealApp> createState() => _MealAppState();
+}
+
+class _MealAppState extends State<MealApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:uni_life_exam/app/app.dart';
+
+void main() {
+  runApp(MealApp());
+}
